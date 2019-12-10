1c492b6afdb704b29692e9d1513885db09200616
Leonardo Francia Rios <leonardo.franciarios@outlook.com>
2019-12-10 12:31:32 -0500

    change

--- EncryptVsHash.Service/Startup.cs
+++ EncryptVsHash.Service/Startup.cs
@@ -18,7 +18,7 @@ namespace EncryptVsHash.Service
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            Configuration = configuration; 
         }
 
         public IConfiguration Configuration { get; }
